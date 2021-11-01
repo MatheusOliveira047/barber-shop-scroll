d985f66c4b1d7d7cdfb4ae75a0eac6fe35ecdb88
Matheus Oliveira <86749030+MatheusOliveira047@users.noreply.github.com>
2021-10-31 23:23:56 -0300

    commit init

---
+++ js/index.js
@@ -0,0 +1,67 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-banner', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-time', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-experience', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-services', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-services1', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-agendamento', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-agendamento1', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-gallary', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-gallary1', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-map', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-map1', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-footer', {
+        duration: 3000,
+        origin:'bottom',
+        distance: '-100px'
+    });
